dc4194ec34385c141e0c9ab856694c25e2715560
Tony Rowan <trowan812@gmail.com>
2023-02-08 21:19:39 +0000

    First run, fetch name and ev yield

---
+++ lib/ev_collector.rb
@@ -0,0 +1,36 @@
+require "poke-api-v2"
+require "yaml"
+
+class EVCollector
+  def self.run
+    new.run
+  end
+
+  def run
+    page = 1
+
+    stats = []
+    loop do
+      page_results = PokeApi.get(pokemon: {limit: 20, page: page})
+      stats += page_results.results.map { _1.get }
+      page += 1
+      break unless page_results.next_url
+      sleep 1
+    end
+
+    slim_stats = stats.map do |pokemon|
+      {
+        "number" => pokemon.id,
+        "name" => pokemon.name,
+        "ev_yield" => pokemon.stats
+          .select { _1.effort > 0 }
+          .map { [_1.stat.name, _1.effort] }
+          .to_h
+      }
+    end
+
+    File.open("./output.yml", "w") do |file|
+      file.write(YAML.dump(slim_stats))
+    end
+  end
+end
