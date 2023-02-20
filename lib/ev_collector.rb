944bbae974b60e734d8f999e281da6db685453de
Tony Rowan <trowan812@gmail.com>
2023-02-20 20:07:45 +0000

    Make naming of keys consistent

--- lib/ev_collector.rb
+++ lib/ev_collector.rb
@@ -4,18 +4,18 @@ require "yaml"
 require_relative "./poke_api"
 
 class EVCollector
-  def self.run
-    new.run
+  def self.run(...)
+    new.run(...)
   end
 
-  def run
-    statistics = PokeApi.get_all(:pokemon).map do |pokemon|
+  def run(verbose: false)
+    statistics = PokeApi.get_all(:pokemon, verbose: verbose).map do |pokemon|
       {
         "number" => pokemon.id,
         "name" => pokemon.name,
         "ev_yield" => pokemon.stats
           .select { _1.effort > 0 }
-          .to_h { [_1.stat.name, _1.effort] }
+          .to_h { [_1.stat.name.gsub("-", "_"), _1.effort] }
       }
     end
 
--- lib/poke_api.rb
+++ lib/poke_api.rb
@@ -1,12 +1,22 @@
 module PokeApi
-  def self.get_all(resource, sleep_seconds: 1)
+  def self.get_all(resource, page_size: 100, sleep_seconds: 1, verbose: false)
     page = 1
     results = []
+    total_count = nil
 
     loop do
-      page_results = PokeApi.get(resource => {limit: 20, page: page})
+      if verbose
+        if total_count
+          puts "Fetching page #{page} of #{(total_count + page_size / 2) / page_size}"
+        else
+          puts "Fetching page #{page}"
+        end
+      end
+
+      page_results = PokeApi.get(resource => {limit: page_size, page: page})
       results += page_results.results.map { _1.get }
       page += 1
+      total_count = page_results.count
 
       break unless page_results.next_url
 
